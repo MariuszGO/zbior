937a0ae6d09051e342d2f745319a270403bb89ef
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2024-09-12 09:20:47 +0200

    Dodaj pliki projektów.

---
+++ zbior/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zbior
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            wynik.Content = " ";
+            Int32 licz_a, licz_b,tmp;
+
+           
+
+
+            if (Int32.TryParse(liczba_a.Text, out licz_a) && Int32.TryParse(liczba_b.Text, out licz_b))
+            {
+                if (licz_a > licz_b)
+                {
+                    tmp = licz_a;
+                    licz_a = licz_b;
+                    licz_b = tmp;
+                }
+
+                    for (int i = licz_a; i <= licz_b; i++)
+                {
+                    wynik.Content= wynik.Content+ i.ToString() + " ";
+
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Podałeś tekst zamiast liczb", "Błąd");
+            }
+        }
+    }
+}
